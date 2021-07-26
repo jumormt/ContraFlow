f80f2e0bac403e47d720a7a63cafc8336435125c
chengxiao19961022 <769478099@qq.com>
2021-07-26 17:58:31 +0800

    minor fix

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ from src.datas.graph import NodeType
 
 
 def test_ast():
-    file_path = "/home/chengxiao/project/vul_detect/joern_slicer/main.cpp"
-    nodes_path = "/home/chengxiao/project/vul_detect/joern_slicer/output/main.cpp/nodes.csv"
-    edges_path = "/home/chengxiao/project/vul_detect/joern_slicer/output/main.cpp/edges.csv"
+    file_path = "./data/joern/main.cpp"
+    nodes_path = "./data/joern/nodes.csv"
+    edges_path = "./data/joern/edges.csv"
     ln_to_ast = build_ln_to_ast(file_path, nodes_path, edges_path)
     ast_graph = ln_to_ast[39]
     tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
